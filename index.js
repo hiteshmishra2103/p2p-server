aeca114553a9befb463a989eae07b717a2eebe69
Hitesh mishra <mishrahitesh854@gmail.com>
2023-12-26 21:35:08 +0530

    third

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "https://p2prealtime.vercel.app/",
+    origin: "https://p2prealtime.vercel.app",
   },
 });
 
